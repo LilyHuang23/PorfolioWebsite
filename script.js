ffe58157f8edff6405f0a50f825c14a1d7147f49
John Doe <l613426210@gmail.com>
2023-05-16 12:16:05 -0600

    Add contact page and style

--- script.js
+++ script.js
@@ -4,17 +4,3 @@ toggleIcon.addEventListener('click', () => {
     toggleIcon.classList.toggle('bx-sun');
     document.body.classList.toggle('dark-mode')
 });
-// contact page
-const inputs = document.querySelectorAll(".input");
-function focusFunc() {
-    let parent = this.parentNote;
-    parent.classList.add("focus");
-}
-function blurFunc() {
-    let parent = this.parentNote;
-    parent.classList.remove("focus");
-}
-inputs.forEach((input) => {
-    input.addEventListener("focus", focusFunc);
-    input.addEventListener("blur", blurFunc);
-});
